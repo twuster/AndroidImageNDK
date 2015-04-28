7d51e7a35e04c484d939e16d4e69dd6077853e34
Tony Wu <tony.wu@berkeley.edu>
2015-04-28 16:26:43 -0700

    gray works but not edges

--- jni/hello-jni.c
+++ jni/hello-jni.c
@@ -214,6 +214,7 @@ void Java_com_example_hellojni_HelloJni_parallelConvertToGray(JNIEnv* env,
     for (y=0;y<infocolor.height;y++) {
         argb * line = (argb *) pixelscolor;
         uint8_t * grayline = (uint8_t *) pixelsgray;
+		#pragma omp parallel for
         for (x=0;x<infocolor.width;x++) {
             grayline[x] = 0.3 * line[x].red + 0.59 * line[x].green + 0.11*line[x].blue;
         }
@@ -297,6 +298,115 @@ void Java_com_example_hellojni_HelloJni_findEdges(JNIEnv
     graydata = (uint8_t *) pixelsgray;
     edgedata = (uint8_t *) pixelsedge;
 
+    for (y=0;y<=infogray.height - 1;y++) {
+        for (x=0;x<infogray.width -1;x++) {
+            sumX = 0;
+            sumY = 0;
+            // check boundaries
+            if (y==0 || y == infogray.height-1) {
+                sum = 0;
+            } else if (x == 0 || x == infogray.width -1) {
+                sum = 0;
+            } else {
+                // calc X gradient
+                for (i=-1;i<=1;i++) {
+                    for (j=-1;j<=1;j++) {
+                        sumX += (int) ( (*(graydata + x + i + (y + j)
+* infogray.stride)) * Gx[i+1][j+1]);
+                    }
+                }
+
+                // calc Y gradient
+                for (i=-1;i<=1;i++) {
+                    for (j=-1;j<=1;j++) {
+                        sumY += (int) ( (*(graydata + x + i + (y + j)
+* infogray.stride)) * Gy[i+1][j+1]);
+                    }
+                }
+                sum = abs(sumX) + abs(sumY);
+            }
+
+            if (sum>255) sum = 255;
+            if (sum<0) sum = 0;
+
+            *(edgedata + x + y*infogray.width) = 255 - (uint8_t) sum;
+        }
+    }
+
+    AndroidBitmap_unlockPixels(env, bitmapgray);
+    AndroidBitmap_unlockPixels(env, bitmapedges);
+}
+
+
+/*
+findEdges
+Matrix operation
+*/
+void Java_com_example_hellojni_HelloJni_parallelFindEdges(JNIEnv
+* env, jobject  obj, jobject bitmapgray,jobject bitmapedges)
+{
+    AndroidBitmapInfo  infogray;
+    void*              pixelsgray;
+    AndroidBitmapInfo  infoedges;
+    void*              pixelsedge;
+    int                ret;
+    int             y;
+    int             x;
+    int             sumX,sumY,sum;
+    int             i,j;
+    int                Gx[3][3];
+    int                Gy[3][3];
+    uint8_t            *graydata;
+    uint8_t            *edgedata;
+
+    LOGI("findEdges running");
+
+    Gx[0][0] = -1;Gx[0][1] = 0;Gx[0][2] = 1;
+    Gx[1][0] = -2;Gx[1][1] = 0;Gx[1][2] = 2;
+    Gx[2][0] = -1;Gx[2][1] = 0;Gx[2][2] = 1;
+
+    Gy[0][0] = 1;Gy[0][1] = 2;Gy[0][2] = 1;
+    Gy[1][0] = 0;Gy[1][1] = 0;Gy[1][2] = 0;
+    Gy[2][0] = -1;Gy[2][1] = -2;Gy[2][2] = -1;
+
+    if ((ret = AndroidBitmap_getInfo(env, bitmapgray, &infogray)) < 0) {
+        LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
+        return;
+    }
+
+    if ((ret = AndroidBitmap_getInfo(env, bitmapedges, &infoedges)) < 0) {
+        LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
+        return;
+    }
+
+    LOGI("gray image :: width is %d; height is %d; stride is %d; format is %d;flags is %d",infogray.width,infogray.height,infogray.stride,infogray.format,infogray.flags);
+    if (infogray.format != ANDROID_BITMAP_FORMAT_A_8) {
+        LOGE("Bitmap format is not A_8 !");
+        return;
+    }
+
+    LOGI("color image :: width is %d; height is %d; stride is %d; format is %d;flags is %d",infoedges.width,infoedges.height,infoedges.stride,infoedges.format,infoedges.flags);
+    if (infoedges.format != ANDROID_BITMAP_FORMAT_A_8) {
+        LOGE("Bitmap format is not A_8 !");
+        return;
+    }
+
+    if ((ret = AndroidBitmap_lockPixels(env, bitmapgray, &pixelsgray)) < 0) {
+        LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+    }
+
+    if ((ret = AndroidBitmap_lockPixels(env, bitmapedges, &pixelsedge)) < 0) {
+        LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+    }
+
+    // modify pixels with image processing algorithm
+
+    LOGI("time to modify pixels....");
+
+    graydata = (uint8_t *) pixelsgray;
+    edgedata = (uint8_t *) pixelsedge;
+
+	#pragma omp parallel for
     for (y=0;y<=infogray.height - 1;y++) {
         for (x=0;x<infogray.width -1;x++) {
             sumX = 0;
